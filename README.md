3778717f0c6d5d5f0c523f267c2467c753920de5
Lukas Bestle <lukas@lu-x.me>
2014-01-02 20:32:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Flexisel
+
+> Simple carousel without any complicated stuff around it
+
+## Why
+
+You all know these huge jQuery carousel plugins, right?  
+Besides these 20kB or even more, you also have to include jQuery in your site.
+
+I found out that I don't need jQuery in my daily life - it would make my life easier but I really don't like overhead.
+
+So I decided to write basic JS libraries not requiring any other libraries.  
+Also, I want to learn advanced JS, so building this without jQuery was good for me (see "Feedback" below).
+
+## Features
+
+- based on a list of links which can include just about any HTML content (images, text etc.) and a "stage" where the active item is transferred to
+- hides the stage using a custom class name, which will get attached when the stage changes (you will do something like `opacity: 0;` for that class)
+- automatically rotates the items in a specified interval
+- can be triggered manually by the user (clicking an item) or by the developer (`Flexisel.set()` method)
+- can exist multiple times on a page
+- doesn't annoy the user:
+	- doesn't change automatically if the user is hovering the carousel
+	- doesn't change for a while after the user manually clicked on an item
+
+## Flexisel is:
+
+- a simple carousel framework
+- less than 900 bytes minified and gzipped (!)
+- totally flexible, as it doesn't require any other libraries
+
+## Flexisel is *not*:
+
+- packed with any CSS or images, so it can be highly configured
+- one of these amazing 3D rotation carousels as the possible items are separate from the "stage"
+- usable out-of-the-box
+- working in stone-age browsers (requires `document.querySelector` and `Function.bind()`)
+
+## Usage
+
+### HTML
+
+```html
+<section id="carousel">
+	<ul>
+		<li class="active">
+			<a href="http://example.com/this/is/the/destination/of/the/item">
+				<!-- Whatever -->
+			</a>
+		</li>
+		<li>
+			<a href="http://example.com/this/is/another/destination/of/the/item">
+				<!-- Whatever -->
+			</a>
+		</li>
+		<li>
+			<a href="http://example.com/this/is/another/destination/of/the/item">
+				<!-- Whatever -->
+			</a>
+		</li>
+	</ul>
+	<div class="stage">
+		<a href="http://example.com/this/is/the/destination/of/the/item">
+			<!-- Whatever -->
+		</a>
+	</div>
+</section>
+```
+
+### JS
+
+```javascript
+var carousel = new Flexisel(document.getElementById('carousel'));
+```
+
+#### With custom configuration (see `flexisel.js` for documentation)
+
+```javascript
+var carousel = new Flexisel(document.getElementById('carousel'), {
+	'debug':       true,
+	'setCallback': function(link) { ... }
+});
+```
+
+## Feedback
+
+I'm truly not a JS professional - but I want to become one.
+
+I tried to comment everything in the Flexisel code, so if you want to support me and are good at JS, I would be really happy if you would give me [feedback on my code](https://twitter.com/lukasbestle).  
+**Thanks!**
---
+++ flexisel.js
@@ -0,0 +1,178 @@
+/**
+ * Flexisel
+ * Simple carousel without any complicated stuff around it
+ * 
+ * @version   0.8 (02.01.2014)
+ * @author    Lukas Bestle <lukas@lu-x.me>
+ * @copyright Lukas Bestle
+ * @link      https://github.com/vis7mac/flexisel
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * 
+ * @param  object carousel The object of the element you want to init the carousel in
+ * @param  object options  Object with configuration options overwriting the default ones
+ */
+var Flexisel = function(carousel, options) {
+	'use strict';
+	
+	var self                 = this,
+	    attrName,
+	    automaticIteration,
+	    stage,
+	    listItems,
+	    buttons,
+	    addClass,
+	    removeClass;
+	
+	// Expose default options
+	this.options = {
+		// Times
+		interval:           8000,       // Change interval
+		waitAfterManual:    20000,      // Time to wait after an item was manually clicked
+		changeTime:         400,        // Time to wait between hiding and showing the stage
+		
+		// Selectors
+		stage:              '.stage',   // Selector of the stage within a carousel
+		listItems:          'li',       // Possible list items
+		buttons:            'li a',     // Buttons in the item list which are transferred to the stage
+		
+		// Class names
+		stageChangingClass: 'changing', // Class attached to the stage while changing its content
+		activeClass:        'active',   // Class attached to the active list item
+		
+		// Callbacks
+		setCallback:        null,       // Callback called with the object of the current link when setting the current item;
+		                                // can return false to prevent setting an item
+		
+		// Other options
+		debug:              false       // Debug mode (print to console each time the item is changed)
+	};
+	
+	// Merge with custom options
+	if(typeof options === 'object') {
+		for(attrName in options) {
+			if(options.hasOwnProperty(attrName)) {
+				this.options[attrName] = options[attrName];
+			}
+		}
+	}
+	
+	// Get required stuff from the DOM
+	stage     =                            carousel.querySelector   (this.options.stage);
+	listItems = Array.prototype.slice.call(carousel.querySelectorAll(this.options.listItems));
+	buttons   = Array.prototype.slice.call(carousel.querySelectorAll(this.options.buttons));
+	
+	// Expose status stuff
+	this.current         = 0;     // Current item ID in "buttons" (zero-based)
+	this.lastManual      = 0;     // Last time stamp when the user clicked manually
+	this.hovered         = false; // Is the user currently hovering the carousel?
+	
+	/**
+	 * Set the carousel to show a specific item
+	 * 
+	 * @param  object link The object of the item in the list which should be transferred; if not given: "next item"
+	 * @return boolean     Did it work?
+	 */
+	this.set = function(link) {
+		// If no specific link is given, just increase by one
+		var linkObject = (link === undefined)? buttons[(this.current === buttons.length - 1)? 0 : this.current + 1] : link;
+		
+		// Ask the callback if we should do it right now
+		if(typeof this.options.setCallback === 'function' && !this.options.setCallback(link)) {
+			return false;
+		}
+		
+		// Check if the link is the current one
+		if(buttons.indexOf(linkObject) === this.current) {
+			return false;
+		}
+		
+		// Disable all other items, enable the new item
+		listItems.forEach(function(item) {
+			removeClass(item, this.options.activeClass);
+		}.bind(this));
+		addClass(linkObject.parentNode, this.options.activeClass);
+		
+		// Hide the old content
+		addClass(stage, this.options.stageChangingClass);
+		
+		setTimeout(function() {
+			// Load the new content
+			stage.innerHTML = linkObject.outerHTML;
+			
+			// Show the stage again
+			setTimeout(function() {
+				removeClass(stage, this.options.stageChangingClass);
+			}.bind(this), this.options.changeTime / 2);
+		}.bind(this), this.options.changeTime / 2);
+		
+		// Save the current item
+		this.current = buttons.indexOf(linkObject);
+		
+		// Debug
+		if(this.options.debug) {
+			console.log('[Flexisel] Changed to item ' + this.current + ', which was ' + ((link === undefined)? 'the next item' : 'requested') + '.');
+		}
+		
+		return true;
+	};
+	
+	/**
+	 * Change the current item every options.interval milliseconds
+	 * Only if the user is not hovering and did not interact for a while
+	 */
+	automaticIteration = function() {
+		// Wait if the user just clicked
+		var currentTime = new Date().getTime(),
+		    time = (this.lastManual + this.options.waitAfterManual > currentTime)? this.options.waitAfterManual - (currentTime - this.lastManual) : 0;
+		
+		// Wait until that time is over before actually changing
+		setTimeout(function() {
+			// Check if the user is currently hovering or this timeout was set before the user clicked manually
+			if(this.lastManual + this.options.waitAfterManual <= new Date().getTime() && !this.hovered) {
+				// No, all is fine, change to the next item
+				this.set();
+			}
+			
+			// Change again after the interval is over
+			setTimeout(automaticIteration.bind(this), this.options.interval);
+		}.bind(this), time);
+	};
+	setTimeout(automaticIteration.bind(this), this.options.interval);
+	
+	/**
+	 * Manual change
+	 */
+	buttons.forEach(function(item) {
+		item.onclick = function() {
+			self.set(this);
+			
+			// Stop automatic rotation for a time
+			self.lastManual = new Date().getTime();
+		
+			// Prevent reloading the page
+			return false;
+		};
+	}.bind(this));
+	
+	/**
+	 * Detect hovers
+	 */
+	carousel.onmouseover = function() {
+		this.hovered = true;
+	}.bind(this);
+	
+	carousel.onmouseout = function() {
+		this.hovered = false;
+	}.bind(this);
+	
+	/**
+	 * Helper functions
+	 */
+	addClass = function(element, className) {
+		element.className += ((element.className)? ' ' : '') + className;
+	};
+
+	removeClass = function(element, className) {
+		element.className = element.className.replace(new RegExp('(\\s|^)' + className + '(\\s|$)'), ' ').replace(/^\s+|\s+$/g, '');
+	};
+};
